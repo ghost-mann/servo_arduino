5757390d24fc52cc987f41afcb94040e893691b9
ghost-mann <iamaob8@protonmail.com>
2026-01-16 16:52:35 +0300

    feat: added conditional statement to change position of servo by pressing digital buttons in button_servo.cpp

--- src/button_servo.cpp
+++ src/button_servo.cpp
@@ -7,11 +7,27 @@ int button_a = 2;
 int button_b = 3;
 int pos = 0;
 
-void button_servo_setup() {
+//input pullup starts high - when pressed becomes low
 
+void button_servo_setup() {
+    theServo.attach(9);
+    pinMode(button_a,INPUT_PULLUP);
+    pinMode(button_b,INPUT_PULLUP);
 
 }
 
 void button_servo_loop() {
+
+    if (digitalRead(button_a) == LOW) {
+        pos++;
+        delay(5);
+        theServo.write(pos);
+    }
+
+    if (digitalRead(button_b) == LOW) {
+        pos--;
+        delay(5);
+        theServo.write(pos);
+    }
 
 }
