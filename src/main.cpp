92cc94c9417351a916c72ce2058a305c119d75df
ghost-mann <iamaob8@protonmail.com>
2026-01-16 16:40:40 +0300

    feat: moved main.cpp code to servo.cpp, added servo.h header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,12 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo myservo;
 
 void setup() {
-  myservo.attach(9);
-  myservo.write(0);
-  delay(1000);
+
 }
 
 void loop() {
-  for (int i = 0; i <= 180; i++) {
-    myservo.write(i);
-    delay(15);
-  }
 
-  for (int i = 180; i >= 0; i--) {
-    myservo.write(i);
-    delay(15);
-  }
 
 }
---
+++ src/servo.h
@@ -0,0 +1,10 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+void servo_setup();
+void servo_loop();
+
+void button_servo_setup();
+void button_servo_loop();
+
+#endif
