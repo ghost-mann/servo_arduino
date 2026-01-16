550bee50e91d4871795bff2bcba22ee291663dac
ghost-mann <iamaob8@protonmail.com>
2026-01-16 16:36:41 +0300

    feat:added conditional statement to move the servo 180 degrees and back in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,14 @@ void setup() {
 }
 
 void loop() {
+  for (int i = 0; i <= 180; i++) {
+    myservo.write(i);
+    delay(15);
+  }
+
+  for (int i = 180; i >= 0; i--) {
+    myservo.write(i);
+    delay(15);
+  }
 
 }
