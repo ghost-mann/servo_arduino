9f6566c470642544f752f477c66d1c0a4c3a0360
ghost-mann <iamaob8@protonmail.com>
2026-01-16 16:47:02 +0300

    feat: added variables in button_servo.cpp

--- src/button_servo.cpp
+++ src/button_servo.cpp
@@ -3,11 +3,15 @@
 #include "servo.h"
 
 Servo theServo;
+int button_a = 2;
+int button_b = 3;
+int pos = 0;
 
 void button_servo_setup() {
+
 
 }
 
 void button_servo_loop() {
-    
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include "servo.h"
 
 
 void setup() {
+  button_servo_setup();
 
 }
 
 void loop() {
-
+  button_servo_loop();
 
 }
